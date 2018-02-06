244b9f240f1c209d4ca21df42bdab5ac715eb8a9
Akhil Menon <menonakhil90@gmail.com>
2018-02-06 21:51:12 +0530

    Add files via upload

---
+++ models/criterion_five.js
@@ -0,0 +1,141 @@
+// Criterion Five
+let mongoose = require('mongoose');
+
+// Criterion Five Schema
+var criterionFiveSchema = mongoose.Schema({
+  student_support_progression : {
+    iqac_contribution_student_support_services : String,
+
+    efforts_tracking_progression : String,
+
+    total_no_of_students : {
+      ug_students : Number,
+      pg_students : Number,
+      other_students : Number
+    },
+    no_of_students_outside_the_state : Number,
+    no_of_international_students : Number,
+    men : {
+      students_number : Number,
+      percentage : Number
+    },
+    women : {
+      students_number : Number,
+      percentage : Number
+    },
+
+    last_year : {
+      general : Number,
+      sc : Number,
+      st : Number,
+      obc : Number,
+      physically_challenged : Number,
+      students_total : Number
+    },
+    this_year : {
+      general : Number,
+      sc : Number,
+      st : Number,
+      obc : Number,
+      physically_challenged : Number,
+      students_total : Number
+    },
+
+    demand_ratio : Number,
+    dropout_percentage : Number,
+
+    student_support_mechanism : String,
+    no_of_student_beneficiaries : Number,
+
+    no_of_students_qualified_for_examinations : {
+      net : Number,
+      set_slet : Number,
+      gate : Number,
+      cat : Number,
+      ias_ips : Number,
+      state_psc : Number,
+      upsc : Number,
+      others : Number
+    },
+
+    student_counselling_career_guidance : String,
+    no_of_students_benefitted : Number,
+
+    campus_placement_details : {
+      on_campus : {
+        organizations_visited : Number,
+        students_participated : Number,
+        placed_students : Number
+      },
+      off_campus : {
+        placed_students : Number
+      }
+    },
+
+    gender_sensitization_programmes : String,
+
+    students_activities : {
+      participation_sports_games_other_events : {
+        state_uni_level : Number,
+        international_level4 : Number,
+        national_level4 : Number
+      },
+      participation_cultural_events : {
+        state_uni_level1 : Number,
+        international_level5 : Number,
+        national_level5 : Number
+      },
+      no_of_medals_awards : {
+        sports_games : {
+          state_uni_level2 : Number,
+          international_level6 : Number,
+          national_level6 : Number
+        },
+        culturals : {
+          state_uni_level3 : Number,
+          international_level7 : Number,
+          national_level7 : Number
+        }
+      }
+    },
+
+    scholarship_financial_support : {
+      institute_finance_support : {
+        student_no : Number,
+        amount_in_lakhs : Number
+      },
+      govt_financial_support : {
+        student_no : Number,
+        amount_in_lakhs : Number
+      },
+      other_financial_support : {
+        student_no : Number,
+        amount_in_lakhs : Number
+      },
+      international_national_recognition : {
+        student_no : Number,
+        amount_in_lakhs : Number
+      }
+    },
+
+    student_organized_initiatives : {
+      fairs : {
+        state_uni_level4 : Number,
+        international_level8 : Number,
+        national_level8 : Number
+      },
+      exhibition : {
+        state_uni_level5 : Number,
+        international_level9 : Number,
+        national_level9 : Number
+      }
+    },
+
+    social_initiatives_undertaken-by_students : Number,
+
+    major_grievances_of_students : String
+  }
+  retainKeyOrder : true
+});
+
+let CriterionFive = module.exports = mongoose.model('CriterionFive',criterionFiveSchema,'criterion_five');
