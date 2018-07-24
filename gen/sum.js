d5381de422fe1e805305c1cd91d899c01aa0c266
Adarsh M <adarsh.apple@icloud.com>
2018-07-24 14:20:49 +0530

    safe

--- gen/sum.js
+++ gen/sum.js
@@ -1,64 +1,44 @@
 var async = require('async');
 var jsonAdd  = require('json-addition');
 
-let PartAOne = require('../models/part_a1');
-let PartATwo = require('../models/part_a2');
-let CriterionOne = require('../models/criterion_one');
 let CriterionTwo = require('../models/criterion_two');
 let CriterionTwoHoq = require('../models/criterion_two_hoq');
 let CriterionThree = require('../models/criterion_three');
 let CriterionFour = require('../models/criterion_four');
 let CriterionFive = require('../models/criterion_five');
-let CriterionSix = require('../models/criterion_six');
-let CriterionSeven = require('../models/criterion_seven');
-let CriterionEight = require('../models/criterion_eight');
 
 var departments = ['ATM', 'BTC', 'CHE', 'CSE', 'CVE', 'ECE', 'EEE', 'IT', 'MAR', 'MEC'];
 
 function sum() {
-	var CTwos = [], CThrees = [], CFours = [], CFives = [], CSixes = [];
-	var i = 0; 
-	async.eachSeries(departments, function(doc, done) {
-		console.log(doc);
-		i = 0;
-		CriterionTwo.findOne({department: doc}, function(err, doc) {
+	console.log('SUMMING UP')
+	var CTwos = [], CThrees = [], CFours = [], CFives = [];
+
+	async.eachSeries(departments, function(department) {
+		console.log(department);
+		CriterionTwo.findOne({department: department}, function(err, doc) {
 			if(err) console.log(err);
-			else if(doc != null) CTwos.push(doc);
-			++i;
-			if(i == 5) done();
+			else CTwos.push(doc);
 		});
-		CriterionThree.findOne({department: doc}, function(err, doc) {
+		CriterionThree.findOne({department: department}, function(err, doc) {
 			if(err) console.log(err);
-			else if(doc != null) CThrees.push(doc);
-			++i;
-			if(i == 5) done();
+			else CThrees.push(doc);
 		});
-		CriterionFour.findOne({department: doc}, function(err, doc) {
+		CriterionFour.findOne({department: department}, function(err, doc) {
 			if(err) console.log(err);
-			else if(doc != null) CFours.push(doc);
-			++i;
-			if(i == 5) done();
+			else CFours.push(doc);
 		});
-		CriterionFive.findOne({department: doc}, function(err, doc) {
+		CriterionFive.findOne({department: department}, function(err, doc) {
 			if(err) console.log(err);
-			else if(doc != null) CFives.push(doc);
-			++i;
-			if(i == 5) done();
-		});
-		CriterionSix.findOne({department: doc}, function(err, doc) {
-			if(err) console.log(err);
-			else if(doc != null) CSixes.push(doc);
-			++i;
-			if(i == 5) done();
+			else CFives.push(doc);
 		});
 	}, function(err) {
+		console.log('err ' + err)
 		console.log('criterion2 ' + CTwos.length);
 		console.log('criterion3 ' + CThrees.length);
 		console.log('criterion4 ' + CFours.length);
 		console.log('criterion5 ' + CFives.length);
-		console.log('criterion6 ' + CSixes.length);
 
-		var array = CTwos;
+		/* var array = CTwos;
 		var document = jsonAdd.addJSONs(CTwos);
 		document._doc.department = 'HOQ';
 		var pass_dist_arr = [];
@@ -125,7 +105,7 @@ function sum() {
 		delete document._doc["__v"];
 		CriterionSix.update({department: 'HOQ'}, document._doc, { upsert: true }, function(err) {
 			if(err) console.log(err);
-		});
+		}); */
 
 	});
 }
